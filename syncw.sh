730a84282ca4512e0401a9ebaf227aec4e0e0a7f
ns-color <dev.color0007@gmail.com>
2026-03-22 17:53:24 +0530

    Update syncw.sh
    fix typo

--- syncw.sh
+++ syncw.sh
@@ -4,7 +4,7 @@ keybindings_url="https://raw.githubusercontent.com/ns-color/config-dump/refs/hea
 if [[ -e $keybindings_path ]]; then
     echo "Found config"
     curl -sS "$keybindings_url" -o "$file_name"
-    mv "- $file_name" "$keybindings_path"
+    mv "$file_name" "$keybindings_path"
     clear
     echo $keybindings_url
     echo "↴"
