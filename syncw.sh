900a63f508d4094ea8e6ca90fe9280522c27ca30
Color <dev.color0007@gmail.com>
2026-03-22 23:26:09 +0530

    fixed the bug

--- syncw.sh
+++ syncw.sh
@@ -1,15 +1,23 @@
 keybindings_path="$HOME/.config/Code - OSS/User/keybindings.json"
 file_name="keybindings.json"
 keybindings_url="https://raw.githubusercontent.com/ns-color/config-dump/refs/heads/main/keybindings.json"
+masg="Found the config & change has been made."
 if [[ -e $keybindings_path ]]; then
-    echo "Found config"
+    echo "$masg"
     curl -sS "$keybindings_url" -o "$file_name"
     mv "$file_name" "$keybindings_path"
-    clear
-    echo $keybindings_url
-    echo "↴"
-    echo $keybindings_path
-    echo ""
-    echo "Done!"
+else
+    echo "Path not found:"
+    echo "$keybindings_path"
+    touch "$keybindings_path"
+    echo "File '$file_name' is created!"
 
+    if [[ -e $keybindings_path ]]; then
+        echo "$masg"
+        curl -sS "$keybindings_url" -o "$file_name"
+        mv "$file_name" "$keybindings_path"
+        echo ""
+        
+    fi
 fi
+
